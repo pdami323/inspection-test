ed13c3fafb440956fabdcff951447e44679c73eb
ekal0323 <ekal0323@iabacus.co.kr>
2024-07-01 14:13:21 +0900

    최초 작성

---
+++ src/main/java/com/example/inspectiontest/Foo.java
@@ -0,0 +1,12 @@
+package com.example.inspectiontest;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class Foo {
+
+}
+
+
---
+++ src/test/java/com/example/inspectiontest/InspectionTestApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.inspectiontest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class InspectionTestApplicationTests {
+
+
+    @Test
+    void test(){
+        String member1 = "a";
+        String member2 = "b";
+
+        // isEqualTo는 ==, isSameAs는 ref 비교
+//        Assertions.assertThat(member1).isEqualTo(member2);
+    }
+
+}
