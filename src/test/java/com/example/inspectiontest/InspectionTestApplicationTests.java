6cb78d86ede80c3cef73e57aa222d1ae8fd33a81
ekal0323 <ekal0323@iabacus.co.kr>
2024-07-01 14:26:15 +0900

    test

--- src/test/java/com/example/inspectiontest/InspectionTestApplicationTests.java
+++ src/test/java/com/example/inspectiontest/InspectionTestApplicationTests.java
@@ -17,6 +17,7 @@ class InspectionTestApplicationTests {
 
         // isEqualTo는 ==, isSameAs는 ref 비교
 //        Assertions.assertThat(member1).isEqualTo(member2);
+        log.info("test");
     }
 
 }
